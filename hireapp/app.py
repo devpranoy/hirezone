11a5b7951ac87c606444302a0989dfd9ffcdf6b2
Pranoy Dev <devpranoy@gmail.com>
2017-11-21 20:57:41 +0000

    File structure

--- hireapp/app.py
+++ hireapp/app.py
@@ -164,4 +164,4 @@ def logout():
 
 if __name__=='__main__':
 	app.secret_key='secret123' #for flash messaging
-	app.run(host='0.0.0.0',port =80,debug=True) #Debugger is set to 1 for testing and overriding the default port to http port
+	app.run(host='0.0.0.0',port =5000,threaded=True) #Debugger is set to 1 for testing and overriding the default port to http port
